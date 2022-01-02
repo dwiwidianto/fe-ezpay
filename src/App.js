e1a61251e8fb68ba7d510dac4c333f84ab6b7688
widi443 <dwiwidianto00@gmail.com>
2022-01-03 01:19:28 +0700

    create template route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Login, Signup, Home} from './pages'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Login, Signup, Home } from "./pages";
+
+const bg = {
+  "background-color": "black",
+  "display": "flex",
+  "min-height": "100vh",
+  "color": "white"
+}
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-          <Route path="/" exact>{Home}</Route>
+    <div style={bg}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">{Home}</Route>
           <Route path="/login">{Login}</Route>
-          <Route path="/register" >{Signup}</Route>
-      </Switch>
-    </BrowserRouter>
+          <Route path="/register">{Signup}</Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
-}
+};
 
 export default App;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,7 +4,6 @@ export default function Login() {
     return (
         <div>
             <h1>Login page</h1>
-            
         </div>
     )
 }
