b509309a16d1bacea52719fd09bec92605adc5b9
fannanilam <fannanilam10@gmail.com>
2022-01-06 14:56:49 +0700

    update login

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { FloatingLabel, Form, Button, Container } from "react-bootstrap";
+import style from "./signup.module.css";
 
 export default function Signup() {
-    return (
-        <div>
-            <h1>Signup Page</h1>
-        </div>
-    )
+  return (
+    <>
+      <div className={style.login}>EZPAY</div>
+      <Container className={style.card}>
+        <FloatingLabel>
+          <Form>
+            <h2 className={style.signin}>Register</h2>
+            <div className="form-group">
+              <label className={style.label}>Full Name</label>
+              <br />
+              <input type="text" className={style.form}  placeholder="Your full name" />
+            </div>
+            <div className="form-group">
+              <label className={style.label}>Email</label>
+              <br />
+              <input type="email" className={style.form}  placeholder="Your email" />
+            </div>
+            <div className="form-group">
+              <label className={style.label}>Password</label>
+              <br />
+              <input type="password" className={style.form} placeholder="Your password" />
+            </div><br/>
+            <div class="col-md-12 text-center">
+              <Button type="submit" href="/" className={style.button}>
+                Sign Up
+              </Button>
+            </div>
+            <p className="forgot-password text-right" style={{ textAlign: "center" }}>
+              Already have an account?  
+              <Button href="/login" style={{color:"#75F991"}}>
+                  Login
+              </Button>
+            </p>
+          </Form>
+        </FloatingLabel>
+      </Container>
+    </>
+  );
 }
